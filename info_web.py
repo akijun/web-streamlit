26239d7cf35c44b66af2aefc7917ac85c4cb28a6
junaki <jun.akimoto@mbb.nifty.ne.jp>
2022-02-19 16:37:57 +0900

    info_web.py

--- info_web.py
+++ info_web.py
@@ -7,9 +7,9 @@ def page1():
 
     left_column.title("原油　入力")
     in_radio=left_column.radio(
-         "単位",('BPD','kL/年','t/年')
+         "入力単位",('BPD','kL/年','t/年')
         )
-    num=left_column.number_input(in_radio)
+    num=left_column.number_input(in_radio,0)
 
     if in_radio=='BPD':
         num=num
@@ -20,13 +20,13 @@ def page1():
 
     right_column.title("原油　出力")
     out_radio=right_column.radio(
-        "単位",('BPD','KL/年','t/年')
+        "出力単位",('BPD','kL/年','t/年')
     )
     right_column.text(out_radio)
 
     if out_radio=='BPD':
         num=num
-    elif out_radio=='KL/年':
+    elif out_radio=='kL/年':
         num=num*340/6.2898
     elif out_radio=='t/年':
         num=num*340/6.2898/0.8
@@ -38,6 +38,24 @@ def page1():
 
 def page2():
      left_column,right_column=st.columns(2)
+
+     left_column.title("水素　入力")
+     in_radio=left_column.radio(
+         "入力単位",('kg','Nm3')
+     )
+     num2=left_column.number_input(in_radio,0)
+
+     right_column.title("水素　出力")
+     out_radio=right_column.radio(
+         "出力単位",('kg','Nm3')
+     )
+
+     if out_radio=="kg":
+         num2=num2
+
+     right_column.text(out_radio)
+
+     right_column.subheader(num2)
      return
 
 def page3():
